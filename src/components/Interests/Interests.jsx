5147eb0750b1d7c88ba173060699d224ce74dc1d
daanniill <daniil.malakhov12@gmail.com>
2026-06-25 16:25:54 -0700

    redid styling on Interests component so it takes up less space

--- src/components/Interests/Interests.jsx
+++ src/components/Interests/Interests.jsx
@@ -11,34 +11,42 @@ function Interests() {
         Check out our ebay shop <a href="https://www.ebay.com/usr/neovintagetech" target="_blank" rel="noreferrer">here</a>!
       </p>
 
-      <h3 className="interests-subtitle">Favorite Games</h3>
-      <ol className="interests-list">
-        <li className="interests-list-item">The Legend of Zelda: Tears of the Kingdom</li>
-        <li className="interests-list-item">Final Fantasy VII</li>
-        <li className="interests-list-item">The Legend of Zelda: Ocarina of Time</li>
-        <li className="interests-list-item">Monster Hunter World</li>
-        <li className="interests-list-item">Animal Crossing: New Leaf</li>
-        <li className="interests-list-item">Persona 4: Golden</li>
-        <li className="interests-list-item">Horizon Zero Dawn</li>
-      </ol>
+      <div className="interests-lists-grid">
+        <div className="interests-list-group">
+          <h3 className="interests-subtitle">Favorite Games</h3>
+          <ol className="interests-list">
+            <li className="interests-list-item">The Legend of Zelda: Tears of the Kingdom</li>
+            <li className="interests-list-item">Final Fantasy VII</li>
+            <li className="interests-list-item">The Legend of Zelda: Ocarina of Time</li>
+            <li className="interests-list-item">Monster Hunter World</li>
+            <li className="interests-list-item">Animal Crossing: New Leaf</li>
+            <li className="interests-list-item">Persona 4: Golden</li>
+            <li className="interests-list-item">Horizon Zero Dawn</li>
+          </ol>
+        </div>
 
-      <h3 className="interests-subtitle">Favorite Consoles</h3>
-      <ol className="interests-list">
-        <li className="interests-list-item">New Nintendo 3DS</li>
-        <li className="interests-list-item">PlayStation 5</li>
-        <li className="interests-list-item">PlayStation Portable</li>
-        <li className="interests-list-item">Nintendo Wii</li>
-        <li className="interests-list-item">Nintendo Switch</li>
-      </ol>
-      
-      <h3 className="interests-subtitle">Currently Playing</h3>
-      <ol className="interests-list">
-        <li className="interests-list-item">Persona 5 Royal</li>
-        <li className="interests-list-item">Astro Bot</li>
-        <li className="interests-list-item">Monster Hunter Wilds</li>
-        <li className="interests-list-item">The Legend of Zelda: Tears of the Kingdom (again lol)</li>
-        <li className="interests-list-item">Final Fantasy IX</li>
-      </ol>
+        <div className="interests-list-group">
+          <h3 className="interests-subtitle">Favorite Consoles</h3>
+          <ol className="interests-list">
+            <li className="interests-list-item">New Nintendo 3DS</li>
+            <li className="interests-list-item">PlayStation 5</li>
+            <li className="interests-list-item">PlayStation Portable</li>
+            <li className="interests-list-item">Nintendo Wii</li>
+            <li className="interests-list-item">Nintendo Switch</li>
+          </ol>
+        </div>
+
+        <div className="interests-list-group">
+          <h3 className="interests-subtitle">Currently Playing</h3>
+          <ol className="interests-list">
+            <li className="interests-list-item">Persona 5 Royal</li>
+            <li className="interests-list-item">Astro Bot</li>
+            <li className="interests-list-item">Monster Hunter Wilds</li>
+            <li className="interests-list-item">The Legend of Zelda: Tears of the Kingdom (again lol)</li>
+            <li className="interests-list-item">Final Fantasy IX</li>
+          </ol>
+        </div>
+      </div>
 
       <h3 className="interests-subtitle">Photos of my Collection</h3>
       <div className="interests-photos">
